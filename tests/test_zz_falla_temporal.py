2436428c1ed1e51bfbd62c260b236c6977a4ef2e
MarianaPineda0 <mariana.isaza.2004@outlook.com>
2026-08-15 15:06:56 -0500

    🧪 test: eliminar prueba fallida temporal, pipeline validado

--- tests/test_zz_falla_temporal.py
+++
@@ -1,7 +0,0 @@
-# Prueba temporal e intencional para validar que el pipeline bloquea el
-# despliegue cuando una prueba falla. Se elimina inmediatamente despues de
-# confirmar el comportamiento.
-
-
-def test_falla_temporal_para_validar_el_pipeline():
-    assert False
